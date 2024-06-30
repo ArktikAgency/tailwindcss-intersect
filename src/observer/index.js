758d716dc301bd34d219d9b46911646aa2f36075
Jeffrey Kengne <darrelntangu@gmail.com>
2024-06-30 01:12:10 +0100

    replaced "intersect-full, intersect-half" with "intersect-top-50 and intersect-bottom-50"

--- src/observer/index.js
+++ src/observer/index.js
@@ -1,57 +1,78 @@
 const Observer = {
-    start() {
-        if (document.readyState === 'loading') {
-            document.addEventListener('DOMContentLoaded', () => this.observe())
+  start() {
+    if (document.readyState === "loading") {
+      document.addEventListener("DOMContentLoaded", () => this.observe());
+      return;
+    }
+    this.observe();
+  },
 
-            return
-        }
+  observe() {
+    const selectors = [
+      '[class*=" intersect:"]',
+      '[class*=":intersect:"]',
+      '[class^="intersect:"]',
+      '[class="intersect"]',
+      '[class*=" intersect "]',
+      '[class^="intersect "]',
+      '[class$=" intersect"]',
+    ];
 
-        this.observe()
-    },
+    document.querySelectorAll(selectors.join(",")).forEach((element) => {
+      const intersectClass = Array.from(element.classList).find((className) =>
+        className.startsWith("intersect-")
+      );
+      const [vertical, offset] = this.parseIntersectClass(intersectClass || "");
+      let option;
 
-    observe() {
-        const selectors = [
-            '[class*=" intersect:"]',
-            '[class*=":intersect:"]',
-            '[class^="intersect:"]',
-            '[class="intersect"]',
-            '[class*=" intersect "]',
-            '[class^="intersect "]',
-            '[class$=" intersect"]'
-        ]
+      if (vertical === "top") {
+        option = {
+          threshold: 0,
+          rootMargin: `0px 0px -${100 - offset}% 0px`,
+        };
+      } else if (vertical === "bottom") {
+        option = {
+          threshold: 1,
+          rootMargin: `0px 0px -${offset}% 0px`,
+        };
+      } else {
+        option = {
+          threshold: 0,
+          rootMargin: `0px 0px -${100 - 85}% 0px`,
+        };
+      }
 
-        document.querySelectorAll(selectors.join(',')).forEach(element => {
-            const observer = new IntersectionObserver(entries => {
-                entries.forEach(entry => {
-                    if (! entry.isIntersecting) {
-                        element.setAttribute('no-intersect', '')
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (!entry.isIntersecting) {
+            element.setAttribute("no-intersect", "");
 
-                        return
-                    }
+            return;
+          }
 
-                    element.removeAttribute('no-intersect')
+          element.removeAttribute("no-intersect");
 
-                    element.classList.contains('intersect-once') && observer.disconnect()
-                })
-            }, {
-                threshold: this.getThreshold(element),
-            })
+          element.classList.contains("intersect-once") && observer.disconnect();
+        });
+      }, option);
 
-            observer.observe(element)
-        })
-    },
+      observer.observe(element);
+    });
+  },
 
-    getThreshold(element) {
-        if (element.classList.contains('intersect-full')) {
-            return 0.99
-        }
+  parseIntersectClass(className) {
+    const defaultConfig = ["top", "100"];
+    const match = className.match(/intersect-(top|bottom)-(\d+)/);
 
-        if (element.classList.contains('intersect-half')) {
-            return 0.5
-        }
+    if (!match) {
+      return defaultConfig;
+    }
 
-        return 0
-    },
-}
+    console.log("className: ", className);
 
-export default Observer
+    const [, vertical, offset] = match;
+    return [vertical, offset];
+  },
+};
+
+export default Observer;
